45106776462adb405a172444cbe57864757d0716
moopits <laurent.thorigny@orange.fr>
2024-05-07 15:03:10 +0200

    modif structure fichier

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/'
+ 
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+                  </Router>
+    </React.StrictMode>,
+document.getElementById('root')
+)
