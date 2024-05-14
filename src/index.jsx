4f8c62049ddbd44a3118394270b465e9ea7fe425
Laurent Thorigny <laurent.thorigny@orange.fr>
2024-05-14 12:05:25 +0200

    p8-mise ajour des repertoire

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/'
- 
-ReactDOM.render(
-    <React.StrictMode>
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-                  </Router>
-    </React.StrictMode>,
-document.getElementById('root')
+import './styles/index.scss'
+import Home from './page/Home'
+import About from './page/About'
+import NotFound from './page/NotFound'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    </Router>
 )
+
---
+++ src/page/NotFound.jsx
@@ -0,0 +1,7 @@
+const NotFound = () => {
+    return (
+        <div>Error 404</div>
+    )
+}
+
+export default NotFound
