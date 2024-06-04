4577d3d9d35694a80b1515aba1cbbccc33ea570f
Laurent Thorigny <laurent.thorigny@orange.fr>
2024-06-04 15:01:47 +0200

    p8 -- usestate click page About

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,10 +1,20 @@
 import Banner from "../components/Banner"
 import img from "../assets/images/capture2.jpg"
+import React, { useState } from 'react';
+import data from '../assets/json/about.json'
+import UpdateableParagraph from "../components/Menu"
 
 const About = () => {
+    
+
     return (
         <section className="about">
             <Banner img={img} />
+            <section>
+
+                {data.map(item => <UpdateableParagraph /> key={item.id} item={item} />)}
+
+            </section>
         </section>
     )
 }
